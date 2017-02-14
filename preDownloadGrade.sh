d27df33bcf455f1a1b90bc2b8cc9b0b1b1b12b3c
Chan, Danny <danny50610@yahoo.com.tw>
2017-02-14 16:57:24 +0800

    Update preDownloadGrade.sh

--- preDownloadGrade.sh
+++ preDownloadGrade.sh
@@ -2,6 +2,7 @@ version=$1
 
 mkdir gradle-$version
 cd gradle-$version
-gradle wrapper --gradle-version $version
+gradle wrapper --gradle-version $version --distribution-type bin
 ./gradlew tasks
-
+gradle wrapper --gradle-version $version --distribution-type all
+./gradlew tasks
